205e9a23367d06c43729330f077b6314aed26e63
ccqstark <ccqstark@qq.com>
2023-03-06 21:58:48 +0800

    feat: Breathe to recovery

--- Assets/Scripts/Player/PlayerHealthController.cs
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -13,6 +13,12 @@ public class PlayerHealthController : MonoBehaviour
 
     public Image BloodSplatterImage; // 受伤血迹图片
 
+    public float BeginRecoveryTime; // 脱战后开始回血所需时间
+
+    public float BreatheRecoverySpeed; // 呼吸回血速度系数
+    
+    private float timer; // 呼吸回血计时器
+
     void Start()
     {
         CurrentHealth = MaxHealth;
@@ -21,12 +27,22 @@ public class PlayerHealthController : MonoBehaviour
     
     void Update()
     {
+        timer += Time.deltaTime;
+        // 脱战后一段时间开始回血
+        if (timer >= BeginRecoveryTime)
+        {
+            BreatheRecovery();
+        }
         // 血量小于50%时屏幕出现血迹
         BloodSplatterImage.enabled = CurrentHealth <= MaxHealth * 0.5f;
     }
 
+    // 受到伤害
     public void TakeDamage(int damageValue)
     {
+        // 重置计数器
+        timer = 0;
+        // 扣除生命值
         CurrentHealth -= damageValue;
         if (CurrentHealth <= 0)
         {
@@ -34,4 +50,22 @@ public class PlayerHealthController : MonoBehaviour
             Debug.Log("玩家死亡");
         }
     }
+
+    // 增加生命值
+    public void AddHealth(float addValue)
+    {
+        CurrentHealth += (int) addValue;
+        if (CurrentHealth > MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+        }
+    }
+
+    // 呼吸回血
+    private void BreatheRecovery()
+    {
+        if (CurrentHealth == MaxHealth) return;
+        AddHealth(BreatheRecoverySpeed * Time.deltaTime);
+    }
+    
 }
