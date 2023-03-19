af659c5424ce2fc8e5ce7c5fbd70468bde19b506
ccqstark <ccqstark@qq.com>
2023-03-19 20:30:49 +0800

    feat: Finish main menu scene and init three level scenes

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        // 开始游戏，默认进行关卡一
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("LevelTwo");
+        }
+
+        // 退出游戏
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
