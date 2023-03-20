00569c2a55f46f107a3ddd0a64b19321dc051d83
ccqstark <ccqstark@qq.com>
2023-03-20 21:47:10 +0800

    fix: Fix the bug Main Menu no audio and animation after quit from gaming

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,6 +5,13 @@ namespace UI
 {
     public class MainMenu : MonoBehaviour
     {
+        private void Awake()
+        { 
+            // 避免从游戏中退出后没有动画和声音
+            Time.timeScale = 1f;
+            AudioListener.pause = false;
+        }
+        
         // 开始游戏，默认进行关卡一
         public void PlayGame()
         {
