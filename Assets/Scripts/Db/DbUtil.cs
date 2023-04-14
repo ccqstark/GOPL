a58f89680ca0f885b901ac180ae6822b06f142ce
ccqstark <ccqstark@qq.com>
2023-04-15 03:00:16 +0800

    feat: Add Sqlite to read ranking data

---
+++ Assets/Scripts/Db/DbUtil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using UnityEngine;
+using Mono.Data.Sqlite;
+
+namespace Db
+{
+    public class DbUtil
+    {
+        private static string dbPath = Application.dataPath + "/DB/gopl.db";
+
+        private static SqliteConnection dbConn;
+        
+        // 获取数据库连接
+        public static SqliteConnection GetDbConnection()
+        {
+            try
+            {
+                dbConn = new SqliteConnection(
+                    new SqliteConnectionStringBuilder() { DataSource = dbPath }.ToString());
+                dbConn.Open();
+                return dbConn;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"数据库连接异常: {e.Message}");
+                return null;
+            }
+        }
+        
+        // 查询数据
+        public static IDataReader QueryData(string querySQL)
+        {
+            var dbConn = GetDbConnection();
+            IDbCommand readCmnd = dbConn.CreateCommand();
+            readCmnd.CommandText = querySQL;
+            IDataReader dataReader = readCmnd.ExecuteReader();
+            
+            return dataReader;
+        }
+
+        // 关闭连接，释放资源
+        public static void CloseDbConn()
+        {
+            dbConn.Clone();
+        }
+    }
+}
--- Assets/Scripts/UI/RankingTable.cs
+++ Assets/Scripts/UI/RankingTable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Db;
 using TMPro;
 using UnityEngine;
 
@@ -9,22 +10,49 @@ public class RankingTable : MonoBehaviour
 
     private void Start()
     {
+        // 构造SQL语句
+        string querySQL = $@"SELECT u.username, tr.score, tr.time_used 
+FROM top_record AS tr 
+INNER JOIN user AS u ON tr.user_id = u.id
+WHERE tr.level_id = {{0}}
+ORDER BY tr.score DESC
+LIMIT 10";
+        querySQL = String.Format(querySQL, "1");
         // 从数据库中读取数据
-
+        var dataResult = DbUtil.QueryData(querySQL);
+        int i = 1;
         // 将数据添加到表格中
-        for (int i = 0; i < 10; i++)
+        while (dataResult.Read())
         {
             GameObject newRow = Instantiate(RowPrefab, TableContent.transform.position, TableContent.transform.rotation);
-            newRow.name = "Row" + (i + 1);
+            newRow.name = "Row" + (i);
             // 设置父组件
             newRow.transform.SetParent(TableContent.transform);
             newRow.transform.localScale = Vector3.one; // 设置缩放比例 1,1,1
             //设置预设实例中的各个子物体的文本内容
-            newRow.transform.Find("Rank").GetComponent<TMP_Text>().text = "#" + (i + 1);
-            newRow.transform.Find("Username").GetComponent<TMP_Text>().text = "Ccq" ;
-            newRow.transform.Find("Score").GetComponent<TMP_Text>().text = "6666";
-            newRow.transform.Find("TimeUsed").GetComponent<TMP_Text>().text = "00 : 27";
+            newRow.transform.Find("Rank").GetComponent<TMP_Text>().text = "#" + i;
+            newRow.transform.Find("Username").GetComponent<TMP_Text>().text = dataResult[0].ToString();
+            newRow.transform.Find("Score").GetComponent<TMP_Text>().text = dataResult[1].ToString();
+            newRow.transform.Find("TimeUsed").GetComponent<TMP_Text>().text = dataResult[2].ToString();
+            // 前三名赋予特殊颜色
+            switch (i)
+            {
+                case 1:
+                    newRow.transform.Find("Rank").GetComponent<TMP_Text>().color = Color.yellow;
+                    newRow.transform.Find("Rank").GetComponent<TMP_Text>().fontStyle = FontStyles.Bold;
+                    break;
+                case 2:
+                    newRow.transform.Find("Rank").GetComponent<TMP_Text>().color = Color.grey;
+                    newRow.transform.Find("Rank").GetComponent<TMP_Text>().fontStyle = FontStyles.Bold;
+                    break;
+                case 3:
+                    newRow.transform.Find("Rank").GetComponent<TMP_Text>().color = Color.green;
+                    newRow.transform.Find("Rank").GetComponent<TMP_Text>().fontStyle = FontStyles.Bold;
+                    break;
+            }
             newRow.SetActive(true);
+            i++;
         }
+        DbUtil.CloseDbConn();
     }
 }
